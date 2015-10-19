fa174fa189cc52be42c7f981bc8550054e4fce33
caijunrong <biyabi@caijunrong.local>
2015-10-19 10:22:20 +0800

    Initial Commit

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by biyabi on 15/10/19.
+//  Copyright © 2015年 biyabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
